5a0fa31d6ac67fa2bce6e27e36719a2aa338454f
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-02 14:47:43 -0300

    testando spring boot com redis como cache

---
+++ ws/src/main/java/com/cursochat/ws/WsApplication.java
@@ -0,0 +1,18 @@
+package com.cursochat.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class WsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WsApplication.class, args);
+
+
+	}
+
+}
